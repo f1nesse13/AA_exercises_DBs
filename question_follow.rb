0668b272081193cf31f9e7c1ae5631605171702b
Joe Pell <jpell3130@gmail.com>
2019-03-03 20:38:32 -0500

    updated requires and skeletons for reply and question_like

--- question_follow.rb
+++ question_follow.rb
@@ -1,6 +1,8 @@
 require_relative 'questions_database'
 require_relative 'user'
 require_relative 'question'
+require_relative 'reply'
+require_relative 'question_like'
 
 class QuestionFollow
 
---
+++ question_like.rb
@@ -0,0 +1,5 @@
+require_relative 'questions_database'
+require_relative 'user'
+require_relative 'question'
+require_relative 'reply'
+require_relative 'question_follow'
