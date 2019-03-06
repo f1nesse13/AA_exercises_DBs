ee73004860e2386ede185fd793e328b83cc81622
Joe Pell <jpell3130@gmail.com>
2019-03-05 19:23:31 -0500

    added query to QuestionFollow to list n most followed questions

--- question_follow.rb
+++ question_follow.rb
@@ -28,10 +28,18 @@ class QuestionFollow
     SQL
     data.empty? ? nil : data.map { |datum| Question.new(datum) }
   end
+
+  def self.most_followed_questions(n)
+    data = QuestionsDatabase.instance.execute(<<-SQL, n: n)
+    SELECT DISTINCT questions.* FROM questions JOIN question_follows ON question_follows.question_id = questions.id GROUP BY questions.id ORDER BY COUNT(question_follows.question_id) DESC LIMIT :n
+    SQL
+    data.map { |datum| Question.new(datum) }
+  end
   
   def initialize(options)
     @id = options['id']
     @user_id = options['user_id']
     @question_id = options['question_id']
   end
+
 end
